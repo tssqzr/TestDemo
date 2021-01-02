54ff12a24e1234b52c2e41eed1b5320bc30ccebd
stian02 <stian02@glprop.com>
2021-01-03 00:30:15 +0800

    clear

--- TestMaven/src/main/java/TestMaven/TestMaven/App.java
+++ TestMaven/src/main/java/TestMaven/TestMaven/App.java
@@ -10,25 +10,14 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-        System.out.println( "Hel111lo Worl123!222-zt" );
-        System.out.println("zzzzzzzzzzzzzzzzzzzzzzzz");
-
-
-        System.out.println( "2-ts" );
-        System.out.println("TTTTTTTTTTTTTTTTTTTTTTTTTTT11");
     }
     
     static void test() {
     	System.out.println("test");
-    	System.out.println("faiwf adfa");
-    	// fjaif awewjfiejiw
-    	System.out.println("bbbbbbbbbbbbbbbbbbbbbbbbbbbb");
-    	System.out.println("ZZZ");
     }
     
     static void print() {
     	System.out.println("print");
-    	System.out.println("jifewajfe ");
     }
 
 }
